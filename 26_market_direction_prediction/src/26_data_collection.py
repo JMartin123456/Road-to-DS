01479e95dde782a881c6a6e3b7a5bab56be9d0e8
JMartin123456 <juraj.martinek@gmail.com>
2026-08-18 09:32:17 +0200

    Eda completed

--- 26_market_direction_prediction/src/26_data_collection.py
+++ 26_market_direction_prediction/src/26_data_collection.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import requests
 import pandas as pd
 
+import matplotlib.pyplot as plt
 
 # ====================================
 # API
@@ -50,7 +51,6 @@ df.columns = [
     "Close"
 ]
 
-
 # ====================================
 # Data types
 # ====================================
@@ -84,6 +84,107 @@ df = df.sort_values(
     "Date"
 )
 
+# ====================================
+# Data Validation
+# ====================================
+
+# print("\nMissing values:")
+# print(df.isna().sum())
+
+# print("\nDuplicate dates:")
+# print(df["Date"].duplicated().sum())
+
+# print("\nData types:")
+# print(df.dtypes)
+
+# print("\nDescriptive statistics:")
+# print(df.describe())
+
+# invalid_high = (
+#     (df["High"] < df["Open"]) |
+#     (df["High"] < df["Close"])
+# ).sum()
+
+# invalid_low = (
+#     (df["Low"] > df["Open"]) |
+#     (df["Low"] > df["Close"])
+# ).sum()
+
+# print("\nOHLC validation:")
+# print("Invalid High values:", invalid_high)
+# print("Invalid Low values:", invalid_low)
+
+# ====================================
+# Returns
+# ====================================
+
+df["Return"] = df["Close"].pct_change()
+
+print("\nReturns:")
+print(df["Return"].describe())
+
+print("\nLargest positive returns:")
+print(
+    df.nlargest(10, "Return")[
+        ["Date", "Close", "Return"]
+    ]
+)
+
+print("\nLargest negative returns:")
+print(
+    df.nsmallest(10, "Return")[
+        ["Date", "Close", "Return"]
+    ]
+)
+
+# ====================================
+# Rolling Volatility
+# ====================================
+
+df["Volatility_20"] = (
+    df["Return"]
+    .rolling(20)
+    .std()
+)
+
+print("\n20-day Rolling Volatility:")
+print(df["Volatility_20"].describe())
+
+# ====================================
+# Volatility chart
+# ====================================
+
+plt.figure(figsize=(12, 6))
+
+plt.plot(
+    df["Date"],
+    df["Volatility_20"]
+)
+
+plt.title("EUR/USD 20-Day Rolling Volatility")
+plt.xlabel("Date")
+plt.ylabel("Volatility")
+
+plt.tight_layout()
+plt.show()
+
+# ====================================
+# Return Distribution
+# ====================================
+
+plt.figure(figsize=(10, 6))
+
+plt.hist(
+    df["Return"].dropna(),
+    bins=100
+)
+
+plt.title("Distribution of EUR/USD Daily Returns")
+plt.xlabel("Daily Return")
+plt.ylabel("Frequency")
+
+plt.tight_layout()
+plt.show()
 
 # ====================================
 # Save
@@ -103,6 +204,6 @@ df.to_csv(
     index=False
 )
 
-print(df.head())
-print(df.tail())
-print(df.shape)
+# print(df.head())
+# print(df.tail())
+# print(df.shape)
